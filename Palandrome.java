8af3cdb16462d0028be0db7cc66d1d51c456a5a0
Sreenu827 <sreenu.ramanaboina@gmail.com>
2023-12-24 03:10:46 -0600

    Palandrome Code

---
+++ Palandrome.java
@@ -0,0 +1,29 @@
+public class Palandrome{
+    public static boolean isPalandrome(int x){
+        String str = Integer.toString(x);
+        int i = 0;
+        int j = str.length() -1;
+
+        while (i<j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+
+    public static void main(String []args){
+        int Number = 1222;
+        boolean result = isPalandrome(Number);
+
+        System.out.println("The Given Number : " + Number + " Is a Palandrome? " + result);
+
+        
+
+
+    }
+
+}
